64066600793555c3636cfc8886491bed848f226e
Daniil <frantic915@gmail.com>
2022-02-11 09:14:05 +0400

    Добавьте файлы проекта.

---
+++ Model/Authors.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseHomework
+{
+    public class Authors
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+
+        public IEnumerable<Orders> Orders { get; set; } = Enumerable.Empty<Orders>();
+    }
+}
---
+++ Orders.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseHomework
+{
+    public class Orders
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorID { get; set; }
+        
+        public Authors Author { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name  { get; set; }
+
+        [Required]
+        
+    }
+}
